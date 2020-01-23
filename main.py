d380cbcecd5e7af5df5478fbb0b304afb6ee62d8
hakngrow <56512250+hakngrow@users.noreply.github.com>
2020-01-23 16:45:30 +0800

    2020-01-23

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
-import pprint as pp
+import pandas as pd
 
 import Config as cfg
 
 import utils.AlphaVantageUtils as av
+import utils.PostgresUtils as pg
+import utils.PriceUpdater as pu
 
+_LBL_FUNC = 'func'
+
+_VAL_GET_PRICES = 'get_prices'
+_VAL_UPDATE_PRICES = 'update_prices'
+
+_VAL_GET_SYMBOLS = 'get_symbols'
 
 _LBL_TICKER = 'ticker'
 _LBL_INTERVAL = 'interval'
 _LBL_SIZE = 'size'
+
+
+def get_all_symbols():
+
+    return pd.DataFrame(pg.get_symbols()).to_html()
 
 
 def get_prices(ticker, interval, size):
@@ -17,17 +30,33 @@ def get_prices(ticker, interval, size):
     return prices.to_html()
 
 
+def update_prices(ticker, interval, size):
+
+    created, duplicates = pu.update_price(ticker, interval, size)
+
+    return ticker + ' ' + interval + ': ' + created + ' created, ' + duplicates + ' duplicates'
+
+
 def process_request(request):
 
     request_json = request.get_json()
 
-    if request.args and _LBL_TICKER in request.args:
+    if request.args and _LBL_FUNC in request.args:
+
+        func = request.args.get(_LBL_FUNC)
 
         ticker = request.args.get(_LBL_TICKER)
         interval = request.args.get(_LBL_INTERVAL)
         size = request.args.get(_LBL_SIZE)
 
-        return get_prices(ticker, interval, size)
+        if func == _VAL_GET_SYMBOLS:
+            get_all_symbols()
+
+        elif func == _VAL_GET_PRICES:
+            return get_prices(ticker, interval, size)
+
+        elif func == _VAL_UPDATE_PRICES:
+            return update_prices(ticker, interval, size)
 
     elif request_json and _LBL_TICKER in request_json:
 
--- utils/PriceUpdater.py
+++ utils/PriceUpdater.py
@@ -6,14 +6,25 @@ import utils.PostgresUtils as pg
 
 def update_price(ticker, interval, size):
 
+    count_duplicate = 0
+    count_create = 0
+
     df_prices = av.get_prices(cfg.AV_APIKEY, ticker, interval, size)
 
     for i in range(0, len(df_prices)):
 
         if not pg.is_price_duplicate(ticker, interval, df_prices[pg._COL_DATETIME][i]):
+
+            print(ticker + ' price on ' + str(df_prices[pg._COL_DATETIME][i]) + ' created : ', df_prices.iloc[i, :].values)
             pg.create_prices([df_prices.iloc[i, :].values])
+            count_create += 1
+
         else:
+
             print('Duplicate ' + ticker + ' price on ' + str(df_prices[pg._COL_DATETIME][i]))
+            count_duplicate += 1
+
+    return count_create, count_duplicate
 
 
-update_price(av._TIC_MICROSOFT, av._INT_DAILY, av._SIZE_FULL)
+# update_price(av._TIC_MICROSOFT, av._INT_DAILY, av._SIZE_COMPACT)
