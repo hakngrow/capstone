ccbf6d0fcf98cf1e710a9de023a388191d333b58
hakngrow <56512250+hakngrow@users.noreply.github.com>
2020-01-23 15:55:04 +0800

    2020-01-23 1555

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def get_prices(ticker, interval, size):
 
     prices = av.get_prices(cfg.AV_APIKEY, ticker, interval, size)
 
-    return pp.pformat(prices.to_dict(), indent=4)
+    return prices.to_html()
 
 
 def process_request(request):
